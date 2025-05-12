38524c4daf9fa09a1bbce4315c9887aad33be012
mesterbanana <159573166+mesterbanana@users.noreply.github.com>
2025-05-12 20:31:03 +0300

    Update questions1.py

--- Streamlit_GUI/pages/questions1.py
+++ Streamlit_GUI/pages/questions1.py
@@ -287,7 +287,7 @@ if not st.session_state['form_valid']:
 
 if st.session_state['ready_to_switch']:
     st.session_state['ready_to_switch'] = False
-    st.switch_page("Streamlit_GUI/pages/results1.py")
+    st.switch_page("pages/results1.py")
 
 st.markdown('<div class="button-container" style="margin-top: 30px;">', unsafe_allow_html=True)
 
